ebb35f6562fc83cd02dd4879dcb88ffa569a0565
darters <egorbut300016@gmail.com>
2025-02-25 00:54:04 +0200

    Fix/Crash on inappropriate input

--- main.py
+++ main.py
@@ -14,10 +14,14 @@ def get_correct_sentence():
     sentence = input("Sentence: ")
     response = ai_service.send_sentence_to_ai(sentence)
 
-    corrected_match = re.search(r"1\)\s*(.+)", response).group(1)
-    speaker_match = re.search(r"2\)\s*(.+)", response).group(1)
-    print(f"\n✅ {corrected_match}")
-    print(f"🔊 {speaker_match}")
+    try:
+        corrected_match = re.search(r"1\)\s*(.+)", response).group(1)
+        speaker_match = re.search(r"2\)\s*(.+)", response).group(1)
+        print(f"\n✅ {corrected_match}")
+        print(f"🔊 {speaker_match}")
+    except:
+        print(f"\nSomething went wrong. Message from gpt: {response}")
+
 
 if __name__ == '__main__':
     while True:
